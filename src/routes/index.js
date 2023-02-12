ce40044e670a5916ac2a135eb70ec2f609235506
Your Name <you@example.com>
2023-02-12 23:07:54 +0700

    2. Xử lý errors, validate email và password

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,12 @@
 import user from './user';
 import auth from './auth';
+import { notFound } from '../middlewares/handle_errors';
 
 const initRoutes = (app) => {
     app.use('/api/v1/user', user);
     app.use('/api/v1/auth', auth);
 
-    return app.use('/', (req, res) => {
-        return res.send('SERVER ONN');
-    });
+    app.use(notFound);
 };
 
 module.exports = initRoutes;
